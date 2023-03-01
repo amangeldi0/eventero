44ca1113c0665124cb87082d4786566ada840efa
amangeldi0 <amanimanbekov12@gmail.com>
2023-03-01 23:05:27 +0600

    sing up and sign in widgets, add validator(schemas, rules)

---
+++ src/shared/constants/validations/validate.ts
@@ -0,0 +1,52 @@
+import { RegisterOptions } from 'react-hook-form';
+
+const maxLength = (length: number) => ({
+    value: length,
+    message: `Максимальная длина ${length} символов`,
+});
+
+const minLength = (length: number) => ({
+    value: length,
+    message: `Минимальная длина ${length} символов`,
+});
+
+const patterEmail = {
+    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    message: 'Ведите корректные данные',
+};
+
+const patternPhone = {
+    value: /^996\d{9}$/,
+    message: 'Ведите корректные данные',
+
+};
+
+const required = {
+    value: true,
+    message: 'Обязательное поле',
+};
+
+export const phoneSchema: RegisterOptions = {
+    required,
+    minLength: minLength(12),
+    maxLength: maxLength(12),
+    pattern: patternPhone,
+};
+
+export const emailSchema: RegisterOptions = {
+    required,
+    minLength: minLength(2),
+    maxLength: maxLength(30),
+    pattern: patterEmail,
+};
+
+export const nameSchema: RegisterOptions = {
+    required,
+    minLength: minLength(2),
+    maxLength: maxLength(18),
+};
+
+export const passwordSchema:RegisterOptions = {
+    required,
+    minLength: minLength(8),
+};
