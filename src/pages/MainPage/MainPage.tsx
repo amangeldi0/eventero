95b90f2e6220f1faa81c71d6dac600fd4a3b90c7
aziz <zizomur@yandex.com>
2023-03-02 13:10:08 +0600

    Dropdodwn component added

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,8 @@
-import { Dropdown } from '@/features/Dropdown/Dropdown';
 import { LogoIcon } from '@/shared/assets/LogoIcon';
 
 const MainPage = () => (
     <div>
         Main Page
-        <Dropdown>
-            <div />
-            <p>hello</p>
-        </Dropdown>
     </div>
 );
 
