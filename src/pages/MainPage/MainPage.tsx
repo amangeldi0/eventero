f8a38cd46029c6c856e64a918e0caf95bd4bcdcc
aziz <zizomur@yandex.com>
2023-03-03 15:16:56 +0600

    added event card to the main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,33 @@
+import { EventCard } from '@/features/EventCard/EventCard';
 import { FastSearch } from '@/features/FastSearch/FastSearch';
+import { SearchBar } from '@/features/SearchBar/SearchBar';
 import { Sidebar } from '@/widgets/Sidebar/Sidebar';
 
 import cls from './MainPage.module.scss';
 
 const MainPage = () => (
-    <div className={cls.main}>
+    <div className={cls.mainPage}>
         <FastSearch />
-        <Sidebar />
+        <div className={cls.mainContainer}>
+            <Sidebar />
+            <div className={cls.eventsContainer}>
+                <SearchBar />
+                <div className={cls.eventsLayout}>
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+
+                </div>
+
+            </div>
+
+        </div>
     </div>
 
 );
