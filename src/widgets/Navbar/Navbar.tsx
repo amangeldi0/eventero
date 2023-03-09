6dc71ded21038e064efba703a02dc88ccc037078
amangeldi0 <amanimanbekov12@gmail.com>
2023-03-09 15:56:06 +0600

    split code, adaptive Navbar

--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { SearchBar } from '@/features/SearchBar/SearchBar';
 import { LogoIcon } from '@/shared/assets/LogoIcon';
+import { classnames } from '@/shared/helpers/classnames';
+import { useMediaQuery } from '@/shared/hooks/useMediaQuery';
 import { Layout } from '@/shared/ui/Layout/Layout';
 import { ConditionRender } from '@/widgets/Navbar/ui/ConditionRender';
 
@@ -11,6 +13,9 @@ import cls from './Navbar.module.scss';
 export const Navbar: FC = () => {
     const location = useLocation();
     const jwt = localStorage.getItem('jwt');
+    const [search, setSearch] = useState<boolean>(false);
+    const media = useMediaQuery('(min-width: 1200px)');
+
     return (
         <div className={cls.shadow}>
             <Layout className={cls.layout}>
@@ -18,11 +23,32 @@ export const Navbar: FC = () => {
                     <Link to="/">
                         <LogoIcon className={cls.LogoIcon} />
                     </Link>
-                    {location.pathname !== '/auth' ? <SearchBar /> : ''}
-                    <ConditionRender
-                        jwt={jwt!}
-                        path={location.pathname}
-                    />
+                    {media ? (
+                        <>
+                            <SearchBar
+                                state={search}
+                                setState={setSearch}
+                            />
+                            <ConditionRender
+                                jwt={jwt!}
+                                path={location.pathname}
+                            />
+                        </>
+                    ) : (
+                        <div className={cls.icons}>
+                            <SearchBar
+                                state={search}
+                                setState={setSearch}
+                                className={
+                                    classnames(cls.hideSearch, { [cls.showSearch]: search }, [])
+                                }
+                            />
+                            <ConditionRender
+                                jwt={jwt!}
+                                path={location.pathname}
+                            />
+                        </div>
+                    )}
                 </div>
             </Layout>
         </div>
