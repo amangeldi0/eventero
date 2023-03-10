f15683ffc678cbcf201610eebefe03a4062b4434
amangeldi0 <amanimanbekov12@gmail.com>
2023-03-10 15:24:13 +0600

    adaptive eventCard, adaptive swiper, adaptive layout, change some styles

---
+++ src/shared/constants/swiper/swiperLayout.ts
@@ -0,0 +1,90 @@
+export const swiperLayout = {
+    320: {
+        slidesPerView: 1.05,
+        spaceBetween: 1,
+    },
+    340: {
+        slidesPerView: 1.09,
+        spaceBetween: 1,
+    },
+    360: {
+        slidesPerView: 1.1,
+        spaceBetween: 2,
+    },
+    380: {
+        slidesPerView: 1.2,
+        spaceBetween: 3,
+    },
+    400: {
+        slidesPerView: 1.3,
+        spaceBetween: 5,
+    },
+    460: {
+        slidesPerView: 1.4,
+        spaceBetween: 2,
+    },
+
+    520: {
+        slidesPerView: 1.5,
+        spaceBetween: 10,
+    },
+    600: {
+        slidesPerView: 1.6,
+        spaceBetween: 10,
+    },
+
+    660: {
+        slidesPerView: 1.8,
+        spaceBetween: 10,
+    },
+    730: {
+        slidesPerView: 1.9,
+        spaceBetween: 10,
+    },
+    760: {
+        slidesPerView: 1.95,
+        spaceBetween: 10,
+    },
+    800: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+    },
+    870: {
+        slidesPerView: 2.1,
+        spaceBetween: 10,
+    },
+    920: {
+        slidesPerView: 2.2,
+        spaceBetween: 10,
+    },
+    940: {
+        slidesPerView: 2.3,
+        spaceBetween: 10,
+    },
+    960: {
+        slidesPerView: 2.4,
+        spaceBetween: 10,
+    },
+    980: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+    },
+    1000: {
+        slidesPerView: 2.6,
+        spaceBetween: 10,
+    },
+
+    1030: {
+        slidesPerView: 2.7,
+        spaceBetween: 10,
+    },
+    1040: {
+        slidesPerView: 2.8,
+        spaceBetween: 10,
+    },
+    1052: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+    },
+
+};
