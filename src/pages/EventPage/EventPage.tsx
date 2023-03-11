f118e51e29b599cc15d42665de1556c458d9eacc
amangeldi0 <amanimanbekov12@gmail.com>
2023-03-11 15:28:08 +0600

    adaptive poster, event swiper break points, add swiper global styles,

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FreeMode } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { Contacts } from '@/pages/EventPage/ui/Contacts';
 import { Description } from '@/pages/EventPage/ui/Description';
+import { swiperEventGalleryBreakPoints } from '@/shared/constants/swiper/swiperLayout';
 
+import bakr from '../../shared/assets/bakr.png';
 import { Poster } from './ui/Poster';
 
 import cls from './EventPage.module.scss';
@@ -11,9 +14,19 @@ export const EventPage = () => (
     <div className={cls.eventPage}>
         <div className={cls.eventPageContainer}>
             <Poster />
+            <Swiper
+                className={cls.swiper}
+                freeMode
+                spaceBetween={5}
+                modules={[FreeMode]}
+                breakpoints={swiperEventGalleryBreakPoints}
+            >
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+            </Swiper>
             <div className={cls.InfoContainer}>
                 <Description />
-                <Contacts />
             </div>
         </div>
     </div>
