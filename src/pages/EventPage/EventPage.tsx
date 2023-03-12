1760372f88131620e638f568ebc12dd214fe3938
aziz <zizomur@yandex.com>
2023-03-12 17:30:22 +0600

    event page adaptive refactored

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -12,26 +12,24 @@ import cls from './EventPage.module.scss';
 
 export const EventPage = () => (
     <div className={cls.eventPage}>
-        <div className={cls.eventPageContainer}>
-            <Poster />
-            <div className={cls.swiperContainer}>
-                <Swiper
-                    modules={[A11y, Pagination]}
-                    slidesPerView="auto"
-                    spaceBetween={5}
-                    pagination={{ clickable: true }}
-                >
-                    <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
-                    <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
-                    <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
-                    {/* <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide> */}
-                    {/* <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide> */}
-                </Swiper>
-            </div>
-            <div className={cls.InfoContainer}>
-                <Contacts />
-                <Description />
-            </div>
+        <Poster />
+        <div className={cls.swiperContainer}>
+            <Swiper
+                modules={[A11y, Pagination]}
+                slidesPerView="auto"
+                spaceBetween={5}
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+                <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide>
+                {/* <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide> */}
+                {/* <SwiperSlide><img className={cls.galleryImage} src={bakr} alt="" /></SwiperSlide> */}
+            </Swiper>
+        </div>
+        <div className={cls.InfoContainer}>
+            <Contacts />
+            <Description />
         </div>
     </div>
 );
